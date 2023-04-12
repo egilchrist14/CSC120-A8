d8430ab5ece202ee66f2b2c91fd05b6f5f485271
Your Name <you@example.com>
2023-04-12 18:43:39 -0400

    Added javadoc

--- Wasp.java
+++ Wasp.java
@@ -1,30 +1,55 @@
 import java.util.ArrayList;
 
+/**
+ * Creates an instance of the Wasp class
+ */
 public class Wasp implements Contract{
     ArrayList<String> items = new ArrayList<String>();
     ArrayList<String> actions = new ArrayList<String>();
     int size = 1;
+    int energy = 1;
     
+    /**
+     * "Picks up" at item by adding it to a string array
+     * @param item The item being picked up
+     */
     public void grab(String item){
         items.add(item);
     }
 
+    /**
+     * Drops at item by removing it from a string array
+     * @param item The item being dropped
+     */
     public String drop(String item){
         items.remove(item);
         String dropped = ("You are no longer carrying " + item);
         return dropped;
     }
 
+    /*
+     * Describes an item
+     * @param item The item being described
+     */
     public void examine(String item){
         System.out.println("This is a/an " + item);
     }
 
+    /*
+     * Throws an item and removes it from the array list of items the wasp is carrying and reduces the wasp's energy
+     * @param item The item being thrown/used
+     */
     public void use(String item){
         System.out.println("You just threw your " + item + "!");
         items.remove(item);
+        this.energy = energy - 1;
         System.out.println("You are no longer carrying " + item);
     }
 
+    /*
+     * States whether the wasp can walk in a certain direction
+     * @param direction The direction in which the wasp wants to walk in
+     */
     public boolean walk(String direction){
         if (direction == "down"){
             System.out.println("Sorry, you can't walk down.");
@@ -34,10 +59,19 @@ public class Wasp implements Contract{
         }
     }
     
+    /*
+     * States whether the wasp can fly to a specified coordinate
+     * @param int x The x-coordinate the wasp wants to fly to
+     * @param int y The y-coordinate the wasp wants to fly to
+     */
     public boolean fly(int x, int y){
+        System.out.println("Since you can fly, you can go pretty much anywhere. Hooray for freedom!");
         return true;
     }
 
+    /*
+     * Reduces the wasp's size
+     */
     public Number shrink(){
         Number newSize = size - 1;
         System.out.println("You're smaller now! Your size is " + newSize + ".");
@@ -45,6 +79,9 @@ public class Wasp implements Contract{
         return newSize;
     }
     
+    /*
+     * Increases the wasp's size
+     */
     public Number grow(){
         Number newSize = size + 1;
         System.out.println("You're bigger now! Your size is " + newSize + ".");
@@ -52,10 +89,17 @@ public class Wasp implements Contract{
         return newSize;
     }
 
+    /*
+     * Prints a string which describes the wasp resting and restores some energy
+     */
     public void rest(){
         System.out.println("You lie down to rest for a while but are now vulnerable.");
+        this.energy = energy + 3;
     }
 
+    /*
+     * Undoes the last instance of either shrink() or grow(), depending on which one was called last
+     */
     public void undo(){
         if (actions.get(-1) == "grow"){
             actions.remove(-1);
--- reflection.md
+++ reflection.md
@@ -1,5 +1,8 @@
 Use this file to record your reflection on this assignment.
 
 - Tell me about your class! What does it represent?
+My class represents a wasp in some sort of natural environment where the classes represent the ways it is able to interact with that environment.
 - What additional methods (if any) did you implement alongside those listed in the interface?
+None
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+It took a while to figure out how I wanted undo() to work within the context of my class representing a wasp and with the other methods, but since there was so much freedom with this assignment the advice I'd give is just to be creative with it since there are basically no restrictions and not be afraid to experiment to see what works.
